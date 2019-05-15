374e0560c477f53117eea911c9e21fed637eaccf
Hesham <hesham@clueapps.net>
2019-05-15 22:57:20 +0200

    Initial Commit

---
+++ TextFieldComponent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldComponent
+//
+//  Created by hesham ghalaab on 5/15/19.
+//  Copyright © 2019 hesham ghalaab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
